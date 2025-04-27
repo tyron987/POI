6b5c9505e801275c141e749e8bb732cc8aa94010
iksni <iksnizdas@gmail.com>
2025-04-27 21:58:29 +0200

    Końcowe poprawki

--- Lab.py
+++ Lab.py
@@ -1,13 +1,14 @@
 from scipy.stats import norm
 from csv import writer
 
+# Liczba punktów w chmurze
 num_points = 2000
 
 # Definicje trzech powierzchni
 surfaces = [
     # Nazwa pliku, rozkład: X, Y, Z
     ("Poziom.xyz",   norm(0, 200), norm(0, 20), norm(0.2, 0.05)),
-    ("Pion.xyz",     norm(0, 20),  norm(0, 200), norm(0.2, 0.05)),
+    ("Pion.xyz",     norm(0, 20),  norm(0.2, 0.05), norm(0, 200)),
     ("Cylinder.xyz", norm(0, 20),  norm(0, 20), norm(100, 200)),
 ]
 
@@ -17,6 +18,6 @@ for name, distribution_x, distribution_y, distribution_z in surfaces:
     y = distribution_y.rvs(size=num_points)
     z = distribution_z.rvs(size=num_points)
 
-    with open(name, 'w', encoding='utf-8', newline='\n') as f:
-        csv_writer = writer(f)
+    with open(name, 'w', encoding='utf-8', newline='\n') as file:
+        csv_writer = writer(file)
         csv_writer.writerows(zip(x, y, z))
